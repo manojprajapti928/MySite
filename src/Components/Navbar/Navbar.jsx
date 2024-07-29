7f6aec4787836bbf0731c75bb7751f227437d7b1
manojprajapti928 <158033172+manojprajapti928@users.noreply.github.com>
2024-07-29 13:53:53 +0530

    change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="nav">
-        <img src="./public/manojlogo.png" alt="" />jgygughgh
+        <img src="./public/manojlogo.png" alt="" />
         
         <div className="navlink">
           <Link to="/">Home</Link>
