df832eabb8b8d2c99ae0b9049cc58dc0b9df1a3d
manojprajapti928 <158033172+manojprajapti928@users.noreply.github.com>
2024-07-31 13:28:33 +0530

    cahnge

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+
 export default function Navbar() {
+
+  const [bgColor, setBgColor] = useState({marginTop:"0px"});
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setBgColor({marginTop:"-270px"});
+      }  else if (window.scrollY<100) {
+        setBgColor({marginTop:"0px"});
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+   
+
   return (
     <>
-      <nav className="nav">
+      <nav className="nav"  style={ bgColor }>
         <img src="https://static.vecteezy.com/system/resources/previews/024/553/534/non_2x/lion-head-logo-mascot-wildlife-animal-illustration-generative-ai-png.png" alt="" />
         
         <div className="navlink">
